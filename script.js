34700d6dd01e663a7fcb7fe832b13a4952edfcca
INFHTH <peempos.htc@gmail.com>
2025-03-25 00:36:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const iframe = document.getElementById('proxy-frame');
+    iframe.src = 'https://script.google.com/a/~/macros/s/AKfycbyaVXeDOUaMg3YRw1VhrwEzMTqG2afdt7OD061b47ow0X67EpMQiLjItxFH_POh9WSBGg/exec';
+});
