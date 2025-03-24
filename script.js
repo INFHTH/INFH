87f5b81c2f2cfd18bd8646837dfe8e1d2d58d337
INFHTH <peempos.htc@gmail.com>
2025-03-25 00:38:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+
 document.addEventListener('DOMContentLoaded', () => {
     const iframe = document.getElementById('proxy-frame');
     iframe.src = 'https://script.google.com/a/~/macros/s/AKfycbyaVXeDOUaMg3YRw1VhrwEzMTqG2afdt7OD061b47ow0X67EpMQiLjItxFH_POh9WSBGg/exec';
